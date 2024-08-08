bf5f41789d5fedcfd0dbe76a1990b3a475c1e934
Jonathan Garcia <jdgarcia15@espe.edu.ec>
2024-08-08 05:10:33 -0500

    actualizacion14

--- src/test/java/ec/edu/espe/granjapiscicola/InformacionGeneralStepDefinition.java
+++ src/test/java/ec/edu/espe/granjapiscicola/InformacionGeneralStepDefinition.java
@@ -18,17 +18,17 @@ public class InformacionGeneralStepDefinition extends BasicStepDefinition {
     public void quiero_visualizar_informacion() {
         createPDF("Informacion General");
         addText("Inicio de prueba: Quiero mostrar informacion");
-
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("file:///C:/Users/noobp/Desktop/Requisitos%20(1)/Requisitos/Pagina_Inicial.html");
 
     }
     @When("Ingreso a la pagina web {string} visualizo la informacion inicial")
     public void ingreso_a_la_pagina_web_visualizo_la_informacion_inicial(String pagina){
         addText("Ingreso a la pagina  ");
         pag=pagina;
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.get(pagina);
-        wait(2);
+
+        wait(1);
         captureScreenShot();
 
 
